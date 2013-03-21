74d8bd3c68f9b5b18d082327d9bcf429c697e8bd
Kenaniah Cerny <kenaniah@gmail.com>
2013-03-21 16:17:13 -0700

    Added regexp matching for prev / next links

--- content_script.js
+++ content_script.js
@@ -13,7 +13,7 @@ var links = [].concat(
 );
 
 //Determine the destinations of the rels we care about
-for(var i=0; i < links.length; i++){
+for(var i in links){
 	if(!links[i].rel || !links[i].href) continue;
 	for(var dest in destinations){
 		if(links[i].rel.toLowerCase() == dest){
@@ -22,44 +22,75 @@ for(var i=0; i < links.length; i++){
 	}
 }
 
+//Search for destinations based on link contents
+for(var dest in destinations){
+
+	//Skip destinations that have already been matched
+	if(destinations[dest]) continue;
+
+	//Track an expression matching array
+	var regexes = [];
+
+	if(dest == "next"){
+		regexes.push(/next/i);
+		regexes.push(/^next/i);
+	}
+	if(dest == "prev"){
+		regexes.push(/prev/i);
+		regexes.push(/previous/i);
+		regexes.push(/^prev/i);
+		regexes.push(/^previous/i);
+	}
+
+	//Attempt to match links based on text
+	for(var regex in regexes){
+		for(var i in links){
+			if(links[i].href && links[i].innerText.match(regex)){
+				destinations[dest] = links[i].href;
+			}
+		}
+	}
+
+}
+
 //Keyboard event handler
 function keyListener(e){
-	
+
 	//Ignore anything with alt in it
 	if(e.altKey) return;
-	
+
 	//Ctrl + Left
 	if(e.ctrlKey && !e.shiftKey && e.keyCode == 37 && destinations.prev){
 		window.location = destinations.prev;
 	}
-	
+
 	//Ctrl + Right
 	if(e.ctrlKey && !e.shiftKey && e.keyCode == 39 && destinations.next){
 		window.location = destinations.next;
 	}
-	
+
 	//Ctrl + Up
 	if(e.ctrlKey && !e.shiftKey && e.keyCode == 38 && destinations.up){
 		window.location = destinations.up;
 	}
-	
+
 	//Ctrl + Shift + Up
 	if(e.ctrlKey && e.shiftKey && e.keyCode == 38 && destinations.top){
 		window.location = destinations.top;
 	}
-	
+
 	//Space (when scrolled to the bottom of the window)
 	if(!e.ctrlKey && !e.shiftKey && e.keyCode == 32 && e.srcElement == document.body && destinations.next){
 		if(document.body.scrollHeight - document.body.scrollTop - document.documentElement.clientHeight <= 0){
 			window.location = destinations.next;
 		}
 	}
-	
+
 	//Shift + Space (when scrolled to the top of the window)
 	if(!e.ctrlKey && e.shiftKey && e.keyCode == 32 && e.srcElement == document.body && destinations.prev){
-		if(!document.body.scrollTop) window.location = destinations.prev; 
+		if(!document.body.scrollTop) window.location = destinations.prev;
 	}
-	
+
 }
 
 //The key listening event should only be bound to the top window
